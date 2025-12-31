1b221632659506052d162a063d7ff46ffa347ed7
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-31 16:28:52 +0530

    add simpleMDE

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
 
 
   return (
-    <nav className=' navbar bg-base-100 shadow-sm flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <nav className=' navbar bg-gray-100 shadow-sm flex space-x-6 border-b border-gray-200 mb-5 px-5 h-14 items-center'>
       <Link href="/"><IoBugSharp /></Link>
       <ul className='flex space-x-6'>
         {links.map(link => <Link 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,38 @@
 'use client'
 
-import { TextField,TextArea, Button } from '@radix-ui/themes'
-import React from 'react'
+import { TextField, Button } from '@radix-ui/themes'
+import dynamic from 'next/dynamic';
+// @ts-ignore - CSS side-effect import has no type declarations
+import "easymde/dist/easymde.min.css";
+import { useMemo } from 'react';
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { 
+  ssr: false 
+}) as any;
 
-const newIssuePage = () => {
+const NewIssuePage = () => {
+    const mdeOptions = useMemo(() => {
+        return {
+            autofocus: true,
+            spellChecker: false,
+            placeholder: "Description",
+            status: false,
+            autosave: {
+                enabled: true,
+                uniqueId: "issue-description",
+                delay: 1000,
+            },
+
+            toolbar: ["bold", "italic", "heading", "|", "quote", "unordered-list", "ordered-list", "|", "link", "preview"],
+        };
+    }, []);
     return (
-        <div className='card card-body max-w-xl space-y-3   m-auto bg-gray-100 '>
-            <TextField.Root placeholder="Title">
-            </TextField.Root>
-            <TextArea placeholder="Discription" />
-            <Button>Submit New Issue</Button>
+        <div className='max-w-xl space-y-3 m-auto p-5 bg-gray-100 border border-gray-200 rounded-lg'>
+            <TextField.Root placeholder="Title" />
+
+            <SimpleMDE  options={mdeOptions} />
+            <Button className='cursor-pointer'>Submit New Issue</Button>
         </div>
     )
 }
 
-export default newIssuePage
-
+export default NewIssuePage
