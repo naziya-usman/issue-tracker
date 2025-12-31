1d24e72001492a552faa03a2ecd656267191141a
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-31 13:20:19 +0530

    create issue post api

---
+++ app/lib/prisma.ts
@@ -0,0 +1,15 @@
+
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const DATABASE_URL = process.env.DATABASE_URL as string;
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const adapter = new PrismaMariaDb(DATABASE_URL);
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
