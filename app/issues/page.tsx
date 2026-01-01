88cc5754a38f4c212d2407aeff19dbbaa90af67e
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-01-01 12:17:16 +0530

    Show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
-const Issues = () => {
+import Image from "next/image";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import { prisma } from "../lib/prisma";
+const Issues = async () => {
+    const issues = await prisma.issue.findMany();
     return (
         <div>
-            Issues
+            <div className="pb-5">
+                <Button>
+                    <Link href="/issues/new">New Issue</Link>
+                </Button>
+            </div>
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {
+                        issues.map(issue =>
+                            <Table.Row key={issue.id}>
+                                <Table.Cell>
+                                    {issue.title}
+                                    <div className="block md:hidden text-green-900 text-xs font-bold">{issue.status}</div>
+                                </Table.Cell>
+                                <Table.Cell className="hidden md:table-cell " >{issue.status}</Table.Cell>
+                                <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+                            </Table.Row>
+                        )
+                    }
+                </Table.Body>
+            </Table.Root>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Button><Link href="/issues/new">New Issue</Link> </Button>
       <p>Hello World</p>
     </div>
   );
