b2b7af8b88e936a1d35b6e4908176cd1f628eff2
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-01-02 11:14:44 +0530

    Add edit Button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,12 @@
 
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import { prisma } from '@/app/lib/prisma'
-import { Card, Flex, Heading, Text ,Box } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import ReactMarkDown from 'react-markdown'
+import { prisma } from '@/app/lib/prisma';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import delay from 'delay';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import ReactMarkDown from 'react-markdown';
 type UserIdParams = {
     id: string;
 };
@@ -19,16 +21,24 @@ const IssueDetailPage = async ({ params }: PageProps) => {
     if (!issue) notFound()
     await delay(2000)
     return (
-        <div className='max-w-xl'>
-            <Heading>{issue.title}</Heading>
-            <Flex className='space-x-3' my="2">
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.updatedAt.toDateString()}</Text>
-            </Flex>
-            <Card className='prose' mt='2'>
-                <ReactMarkDown>{issue.description}</ReactMarkDown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: "1", md: "2" }} gap="5">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex className='space-x-3' my="2">
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.updatedAt.toDateString()}</Text>
+                </Flex>
+                <Card className='prose' mt='2'>
+                    <ReactMarkDown>{issue.description}</ReactMarkDown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}> Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 
