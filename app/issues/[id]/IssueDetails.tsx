051f231ea1138073abe4cef2f82c8ec4228dc417
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-01-02 11:39:05 +0530

    Refactor:Apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import type { Issue } from '@/app/generated/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import ReactMarkDown from 'react-markdown';
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+    return (
+        <>
+            <Heading>{issue.title}</Heading>
+            <Flex className="space-x-3" my="2">
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.updatedAt.toDateString()}</Text>
+            </Flex>
+            <Card className="prose" mt="2">
+                <ReactMarkDown>{issue.description}</ReactMarkDown>
+            </Card>
+        </>
+    );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,8 @@
-
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import { prisma } from '@/app/lib/prisma';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import delay from 'delay';
-import Link from 'next/link';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkDown from 'react-markdown';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 type UserIdParams = {
     id: string;
 };
@@ -19,24 +15,13 @@ const IssueDetailPage = async ({ params }: PageProps) => {
         where: { id: parseInt(id) }
     })
     if (!issue) notFound()
-    await delay(2000)
     return (
         <Grid columns={{ initial: "1", md: "2" }} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <Flex className='space-x-3' my="2">
-                    <IssueStatusBadge status={issue.status} />
-                    <Text>{issue.updatedAt.toDateString()}</Text>
-                </Flex>
-                <Card className='prose' mt='2'>
-                    <ReactMarkDown>{issue.description}</ReactMarkDown>
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}> Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
         </Grid>
     )
