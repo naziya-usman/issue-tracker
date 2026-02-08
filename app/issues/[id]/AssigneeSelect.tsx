4d3819fb76ca5366f028b3324db9f991fa6e9501
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-02-08 23:15:48 +0530

    Show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import type { Issue, User } from '@/app/generated/prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 
@@ -20,27 +21,32 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     if (error) return null
 
     return (
-        <Select.Root
-            defaultValue={issue.assignedToUserId || "unassigned"}
-            onValueChange={(userId) => {
-                // Check if the selected value is 'unassigned'
-                const assignedToUserId = userId === "unassigned" ? null : userId;
-                axios.patch(`/api/issue/${issue.id}`, {
-                    assignedToUserId
-                });
-            }}
-        >
-            <Select.Trigger placeholder="Assign..." />
-            <Select.Content>
-                <Select.Group>
-                    <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="unassigned">Unassigned</Select.Item>
-                    {users?.map(user => (
-                        <Select.Item value={user.id} key={user.id}>{user.name}</Select.Item>
-                    ))}
-                </Select.Group>
-            </Select.Content>
-        </Select.Root>
+        <>
+            <Select.Root
+                defaultValue={issue.assignedToUserId || "unassigned"}
+                onValueChange={(userId) => {
+                    // Check if the selected value is 'unassigned'
+                    const assignedToUserId = userId === "unassigned" ? null : userId;
+                    axios.patch(`/api/issue/${issue.id}`, {
+                        assignedToUserId
+                    }).catch(error => {
+                        toast.error("Failed to update assignee");
+                    })
+                }}
+            >
+                <Select.Trigger placeholder="Assign..." />
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Suggestions</Select.Label>
+                        <Select.Item value="unassigned">Unassigned</Select.Item>
+                        {users?.map(user => (
+                            <Select.Item value={user.id} key={user.id}>{user.name}</Select.Item>
+                        ))}
+                    </Select.Group>
+                </Select.Content>
+            </Select.Root>
+            <Toaster />
+        </>
     )
 }
 
