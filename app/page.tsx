fc0d736a0f754c45504d50e2c24a9476e2b3aaa1
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-02-11 14:38:47 +0530

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-// 1. Define the type for the Promise
-interface Props {
-  searchParams: Promise<{ page?: string }>;
-}
-
-export default async function Home({ searchParams }: Props) {
-  // 2. Await the searchParams promise
-  const params = await searchParams;
-  
-  // 3. Convert the string param to a number for your component
-  const currentPage = params.page ? parseInt(params.page) : 1;
-
-  return (
-    <Pagination 
-      itemCount={100} 
-      pageSize={10} 
-      currentPage={currentPage} 
-    />
-  );
+export default async function Home() {
+	return <LatestIssues />;
 }
