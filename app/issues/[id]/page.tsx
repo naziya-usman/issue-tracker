8f8f710b7be0b19578ca35934c9a400dca0995fb
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-02-13 16:09:57 +0530

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,18 +7,19 @@ import DeleteIssueButton from './DeleteIssueButton';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
 import AssigneeSelect from './AssigneeSelect';
+import { cache } from 'react';
+
 type UserIdParams = {
     id: string;
 };
 interface PageProps {
     params: Promise<UserIdParams>;
 }
+const fetchUser = cache(async (issueId: number) => await prisma.issue.findUnique({ where: { id: issueId } }));
 const IssueDetailPage = async ({ params }: PageProps) => {
     const session = await getServerSession(authOptions)
     const { id } = await params
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(id) }
-    })
+    const issue = await fetchUser(parseInt(id))
     if (!issue) notFound()
     return (
         <Grid columns={{ initial: "1", sm: "5" }} gap="5">
@@ -38,7 +39,7 @@ const IssueDetailPage = async ({ params }: PageProps) => {
 
 export async function generateMetadata({ params }: PageProps) {
     const { id } = await params
-    const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } })
+    const issue = await fetchUser(parseInt(id))
     return {
         title: issue ? `Issue Tracker - ${issue.title}` : "Issue Tracker - Issue Not Found",
         description: issue ? `Details of the issue: ${issue.title}` : "The requested issue does not exist.",
