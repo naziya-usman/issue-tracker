11009a3d713dede90ad5842872745c9d8a59f807
Naziya Usman <naziyausmantwaiba@gmail.com>
2026-02-13 22:12:49 +0530

    remove error

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,34 @@
 import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./issueChart";
-
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import { prisma } from "./lib/prisma";
 import type { Metadata } from "next";
 
+export const dynamic = 'force-dynamic';
 export default async function Home() {
-
 	const open = await prisma.issue.count({
-		where: {
-			status: "OPEN"
-		}
-	})
+		where: { status: "OPEN" }
+	});
 	const closed = await prisma.issue.count({
-		where: {
-			status: "CLOSED"
-		}
-	})
+		where: { status: "CLOSED" }
+	});
 	const inProgress = await prisma.issue.count({
-		where: {
-			status: "IN_PROGRESS"
-		}
-	})
+		where: { status: "IN_PROGRESS" }
+	});
+
 	return (
-		<Grid columns={{ initial: "1", md: "2", }} gap='6'>
-			<Flex direction="column" gap="6">
+		<Grid columns={{ initial: "1", md: "2" }} gap="5">
+			<Flex direction="column" gap="5">
 				<IssueSummary open={open} closed={closed} inProgress={inProgress} />
 				<IssueChart open={open} closed={closed} inProgress={inProgress} />
 			</Flex>
 			<LatestIssues />
 		</Grid>
-	)
+	);
 }
 
 export const metadata: Metadata = {
 	title: "Issue Tracker - Dashboard",
 	description: "Overview of the project issues",
-}
+};
